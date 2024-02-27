ecc6a7cac3b34a32be53a988bb50681368fa3c3f
Kókai Roland <dtkjyt@inf.elte.hu>
2024-02-27 22:28:00 +0100

    init: project init

---
+++ fitness-tracker-backend/src/test/java/tech/fitnesstackerbackend/fitnesstrackerbackend/FitnessTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.fitnesstackerbackend.fitnesstrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
